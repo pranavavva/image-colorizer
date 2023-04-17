2f6c8faf6ad3b7933342e1ac19240023d8003462
Pranav Avva <pranav.avva@gmail.com>
2023-04-17 15:53:35 -0400

    gpu4

--- image-colorizer/model2.py
+++ image-colorizer/model2.py
@@ -233,7 +233,7 @@ class LitAutoEncoder(pl.LightningModule):
 
 def main():
     autoencoder = LitAutoEncoder(encoder_class=Encoder, decoder_class=Decoder)
-    trainer = pl.Trainer(max_epochs=128, accelerator="gpu", num_nodes=1, devices=2, strategy="ddp")
+    trainer = pl.Trainer(max_epochs=128, accelerator="gpu", num_nodes=1, devices=4, strategy="ddp")
     trainer.fit(
         autoencoder,
         train_dataloaders=train_dataloader,
